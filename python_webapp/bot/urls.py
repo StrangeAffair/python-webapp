c670d785673c05a53a4876a362a45fee81e3dd0d
Galitskiy Kirill <int33h@mail.ru>
2023-05-02 13:07:16 +0300

    some code updates

--- python_webapp/bot/urls.py
+++ python_webapp/bot/urls.py
@@ -1,6 +1,5 @@
 """
 URL configuration for python_webapp project.
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
@@ -15,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
+from django.views.decorators.csrf import csrf_exempt
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('bot/', include('bot.urls')),
+    path('', csrf_exempt(views.index), name='index'),
 ]
--- python_webapp/python_webapp/settings.py
+++ python_webapp/python_webapp/settings.py
@@ -10,18 +10,22 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
+from dotenv  import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+dotenv_path = os.path.join(BASE_DIR, '.env')
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xcgp=&z0+adjjlo&tj&-s(!m$mi$$1f@kwi@i^jcjit$#sp%mw'
+SECRET_KEY = str(os.getenv("DJANGO_SECRET_KEY"))
+TELEGRAM_BOT_TOKEN = str(os.getenv("TELEGRAM_BOT_TOKEN"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True # TO DO: comment
@@ -38,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bot',
 ]
 
 MIDDLEWARE = [
